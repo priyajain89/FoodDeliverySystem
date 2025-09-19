9da54489ecf413b252886ef9d35e4ba712c5fae9
Shrejal2903 <ShrejalDipak.Raut@cognizant.com>
2025-09-19 15:39:19 +0530

    initial commit

--- FoodDelivery.Infrastructure/DTO/UserDto.cs
+++ FoodDelivery.Infrastructure/DTO/UserDto.cs
@@ -1,18 +1,1 @@
 ﻿
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace FoodDelivery.Infrastructure.DTO
-//{
-//    public class UserDto
-//    {
-//        public string Name { get; set; }
-//        public string Email { get; set; }
-//        public string Phone { get; set; }
-//        public Role Role { get; set; }
-//    }
-
-//}
